aac2fae58414e283dc973756b5e3485c571c1cbb
Paolo Boffi <paolo.boffi@polimi.it>
2024-01-31 15:55:06 +0500

    updated flexibility

--- compute_diff.bat
+++ compute_diff.bat
@@ -1,15 +1,19 @@
-pdflatex main.tex
-bibtex main
-pdflatex main.tex
-pdflatex main.tex
+@echo off
+set /p %main%="Enter the name of the updated %main% file (without extensions): "
+set /p %main_old%="Enter the name of the old %main% file (without extensions): "
 
-pdflatex main_old.tex
-bibtex main_old
-pdflatex main_old.tex
-pdflatex main_old.tex
+pdflatex %main%.tex
+bibtex %main%
+pdflatex %main%.tex
+pdflatex %main%.tex
 
-latexdiff --flatten main_old.tex main.tex > diff.tex
-latexdiff --flatten main_old.bbl main.bbl > diff.bbl
+pdflatex %main_old%.tex
+bibtex %main_old%
+pdflatex %main_old%.tex
+pdflatex %main_old%.tex
+
+latexdiff --flatten %main_old%.tex %main%.tex > diff.tex
+latexdiff --flatten %main_old%.bbl %main%.bbl > diff.bbl
 
 bibtex diff
 pdflatex diff.tex
@@ -20,5 +24,5 @@ del *.log
 del *.out
 del *.blg
 del *.bbl
-del main.pdf
-del main_old.pdf
+del %main%.pdf
+del %main_old%.pdf
